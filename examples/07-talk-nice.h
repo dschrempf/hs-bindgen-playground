02d32e5d6ee97e4cabb9702733723eb4d5fa5e01
Dominik Schrempf <dominik@well-typed.com>
2026-09-24 14:10:21 +0200

    Drop vector_new from the nice talk example

--- examples/07-talk-nice.h
+++ examples/07-talk-nice.h
@@ -9,10 +9,9 @@ typedef enum { LINEAR, CUBIC } interp;
 
 typedef double scalar;
 
-vector  vector_add(vector a, vector b);
-scalar  vector_length(vector v);
-vector *vector_new(double x, double y);
-void    vector_map(vector *vs, size_t n, double (*f)(double));
+vector vector_add(vector a, vector b);
+scalar vector_length(vector v);
+void   vector_map(vector *vs, size_t n, double (*f)(double));
 
 #define VEC_VERSION 2
 #define VEC_EPSILON 1e-9
